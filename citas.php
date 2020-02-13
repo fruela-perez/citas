c002a9429ae95f430f815466f8e89f167cae7d20
fruela-perez <fruela.perez@protonmail.com>
2020-02-14 00:52:01 +0100

    Commit inicial

---
+++ citas.php
@@ -0,0 +1,34 @@
+<?php
+
+	$archivoCitas = __DIR__ . DIRECTORY_SEPARATOR . "citas.txt";
+
+	$total = exec ( "wc -l " . $archivoCitas );		
+	$linea = rand ( 1, $total );
+
+	$fn = fopen ( $archivoCitas , "r" );
+
+	$i = 0;
+	while ( ! feof ( $fn ) )  
+	{
+		$i++;
+		$cita = fgets ( $fn );
+		
+		if ( $i == $linea )
+		{
+			imprimir ( $cita );
+			break;
+		}
+	}
+	fclose ( $fn );
+
+	function imprimir ( $linea )
+	{
+		$fragmentos = explode("#", $linea );
+		
+		echo "\n";
+		echo $fragmentos[1];
+		echo $fragmentos[0];
+	}
+?>
+
+
