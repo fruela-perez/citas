b24d4c96a067307adc7431401cc04c8268788258
fruela-perez <fruela.perez@protonmail.com>
2020-02-16 17:10:10 +0100

    "\n" --> PHP_EOL

--- citas.php
+++ citas.php
@@ -3,7 +3,7 @@
 
 	if ( ! file_exists ( $archivoCitas ) ) die ();
 
-	$total = (int) str_replace( "\n", "", exec ( "wc -l " . $archivoCitas ) );		
+	$total = (int) str_replace( PHP_EOL, "", exec ( "wc -l " . $archivoCitas ) );		
 	$linea = rand ( 1, $total );
 
 	$fn = fopen ( $archivoCitas , "r" );
@@ -26,8 +26,7 @@
 	{
 		$fragmentos = explode ( "#", str_replace ( "\n", "", $linea ) );
 		
-		echo "\n" . wordwrap ( "«" . $fragmentos[1] . "»", str_replace ( "\n", "", shell_exec ( 'tput cols' ) ), "\n", false ) . "\n";
-		echo "\033[1m" . $fragmentos[0] . "\033[0m"; // Autor en negrita
-		echo "\n\n";
+		echo PHP_EOL . wordwrap ( "«" . $fragmentos[1] . "»", str_replace ( PHP_EOL, "", shell_exec ( 'tput cols' ) ), PHP_EOL, false ) . PHP_EOL;
+		echo "\033[1m" . $fragmentos[0] . "\033[0m" . PHP_EOL . PHP_EOL; // Autor en negrita
 	}
 ?>
